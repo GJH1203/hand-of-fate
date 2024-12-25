7dd6cef7db6fe6f18ab323083c924027bc36b49a
Jiahe Gao <90098415+GJH1203@users.noreply.github.com>
2024-12-25 13:01:33 -0600

    Update dependency file

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.flywaydb.flyway") version "9.14.1"
 }
 
 group = "com.cardgame"
@@ -21,10 +22,23 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-logging")
+
+	// immutables
+	annotationProcessor("org.immutables:value:2.10.1")
+	compileOnly("org.immutables:value:2.10.1")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+flyway {
+	url = "jdbc:postgresql://localhost:5432/demo_db"
+	user = "postgres"
+	password = "postgres"
+	schemas = arrayOf("public")
 }
