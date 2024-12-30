15f8c3357ac1af70679595d1231255b039a95d0e
Jiahe Gao <90098415+GJH1203@users.noreply.github.com>
2024-12-29 22:56:35 -0600

    implement GameDto

---
+++ src/main/java/com/cardgame/dto/BoardDto.java
@@ -0,0 +1,4 @@
+package com.cardgame.dto;
+
+public interface BoardDto {
+}
--- src/main/java/com/cardgame/dto/GameDto.java
+++ src/main/java/com/cardgame/dto/GameDto.java
@@ -1,5 +1,21 @@
 package com.cardgame.dto;
 
-public class GameDto {
 
+import com.cardgame.model.GameState;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import org.immutables.value.Value;
+
+import java.time.Instant;
+
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableGameDto.class)
+@JsonDeserialize(as = ImmutableGameDto.class)
+public interface GameDto {
+    String getId();
+    GameState getState();
+    BoardDto getBoard();
+    Instant getCreatedAt();
+    Instant getUpdatedAt();
 }
