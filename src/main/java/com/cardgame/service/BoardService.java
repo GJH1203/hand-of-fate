b06adcaf90e74af58dc6b0391aa9c15b98273de3
Jiahe Gao <90098415+GJH1203@users.noreply.github.com>
2025-01-03 03:12:54 -0600

    implement PositionService and update BoardService with Dto layer

--- src/main/java/com/cardgame/service/BoardService.java
+++ src/main/java/com/cardgame/service/BoardService.java
@@ -1,27 +1,47 @@
 package com.cardgame.service;
 
+import com.cardgame.dto.BoardDto;
+import com.cardgame.dto.ImmutableBoardDto;
+import com.cardgame.dto.ImmutablePositionDto;
 import com.cardgame.model.Board;
 import com.cardgame.model.Position;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class BoardService {
 
-    public Board createBoard(int width, int height, Map<Position, String> pieces) {
-        return new Board(width, height, pieces);
+    public BoardDto createBoard(int width, int height, Map<Position, String> pieces) {
+        Board board = new Board(width, height, pieces);
+        return convertToDto(board);
     }
 
-    public Board updateBoard(Board board, int width, int height, Map<Position, String> pieces) {
+    public BoardDto updateBoard(Board board, int width, int height, Map<Position, String> pieces) {
         board.setWidth(width);
         board.setHeight(height);
         board.setPieces(pieces);
-        return board;
+        return convertToDto(board);
     }
 
     public Map<Position, String> getPieces(Board board) {
         return board.getPieces();
+    }
+
+    private BoardDto convertToDto(Board board) {
+        return ImmutableBoardDto.builder()
+                .width(board.getWidth())
+                .height(board.getHeight())
+                .pieces(board.getPieces().entrySet().stream()
+                        .collect(Collectors.toMap(
+                                entry -> ImmutablePositionDto.builder()
+                                        .x(entry.getKey().getX())
+                                        .y(entry.getKey().getY())
+                                        .build(),
+                                Map.Entry::getValue
+                        )))
+                .build();
     }
 
 }
---
+++ src/main/java/com/cardgame/service/PositionService.java
@@ -0,0 +1,34 @@
+package com.cardgame.service;
+
+import com.cardgame.dto.ImmutablePositionDto;
+import com.cardgame.dto.PositionDto;
+import com.cardgame.model.Position;
+
+public class PositionService {
+
+    public PositionDto createPosition(int x, int y) {
+        Position position = new Position(x, y);
+        return convertToDto(position);
+    }
+
+    public PositionDto updatePosition(Position position, int x, int y) {
+        position.setX(x);
+        position.setY(y);
+        return convertToDto(position);
+    }
+
+    public int getX(Position position) {
+        return position.getX();
+    }
+
+    public int getY(Position position) {
+        return position.getY();
+    }
+
+    private PositionDto convertToDto(Position position) {
+        return ImmutablePositionDto.builder()
+                .x(position.getX())
+                .y(position.getY())
+                .build();
+    }
+}
