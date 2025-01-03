92d375bb2baa18e1f8c05aa2183fdfc5e59cd61b
Jiahe Gao <90098415+GJH1203@users.noreply.github.com>
2025-01-03 02:51:04 -0600

    update GameRepository, fix the error

--- src/main/java/com/cardgame/repository/GameRepository.java
+++ src/main/java/com/cardgame/repository/GameRepository.java
@@ -3,10 +3,11 @@ package com.cardgame.repository;
 import com.cardgame.model.GameModel;
 import com.cardgame.model.GameState;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface GameRepository extends MongoRepository {
-    List<GameModel> findByState(GameState state);
-    List<GameModel> findByStateOrderByCreatedAtDesc(GameState state);
+public interface GameRepository extends MongoRepository<GameModel, String> {
+//    List<GameModel> findByState(GameState state);
+//    List<GameModel> findByStateOrderByCreatedAtDesc(GameState state);
 }
