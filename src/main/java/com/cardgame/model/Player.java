39101ba8cfa4161fc37b758d1f60f926b10d2be0
Jiahe Gao <90098415+GJH1203@users.noreply.github.com>
2025-01-13 02:38:30 -0600

    implement Player model and dto

---
+++ src/main/java/com/cardgame/model/Player.java
@@ -0,0 +1,88 @@
+package com.cardgame.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Document(collection = "players")
+public class Player {
+
+    @Id
+    private String id;
+
+    @Field
+    private String name;
+
+    @DBRef
+    private Deck currentDeck;
+
+    @Field("hand")
+    private List<Card> hand = new ArrayList<>();
+
+    @Field("score")
+    private int score;
+
+    @Field("placed_cards")
+    private Map<Position, Card> placedCards = new HashMap<>();
+
+    // Default constructor
+    public Player() {
+    }
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Deck getCurrentDeck() {
+        return currentDeck;
+    }
+
+    public void setCurrentDeck(Deck currentDeck) {
+        this.currentDeck = currentDeck;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(List<Card> hand) {
+        this.hand = hand;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Map<Position, Card> getPlacedCards() {
+        return placedCards;
+    }
+
+    public void setPlacedCards(Map<Position, Card> placedCards) {
+        this.placedCards = placedCards;
+    }
+
+
+}
